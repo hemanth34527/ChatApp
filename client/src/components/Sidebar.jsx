9bd98f694068618981d27db87149d16756aa9a1b
Hemanth Gopal <hemanthgopal1234@gmail.com>
2025-07-03 21:44:48 +0530

    init

---
+++ client/src/components/Sidebar.jsx
@@ -0,0 +1,140 @@
+import React, { useContext, useEffect, useRef, useState } from 'react';
+import assets from '../assets/assets';
+import { useNavigate } from 'react-router-dom';
+import { AuthContext } from '../../context/AuthContext';
+import { ChatContext } from '../../context/chatContext';
+
+const Sidebar = () => {
+  const {
+    getUsers,
+    users,
+    selectedUser,
+    setSelectedUser,
+    unseenMessages,
+    setUnseenMessages,
+  } = useContext(ChatContext);
+
+  const [input, setInput] = useState('');
+  const [menuOpen, setMenuOpen] = useState(false);
+  const { logout, onlineUsers } = useContext(AuthContext);
+  const navigate = useNavigate();
+  const menuRef = useRef();
+
+  useEffect(() => {
+    const handleClickOutside = (e) => {
+      if (menuRef.current && !menuRef.current.contains(e.target)) {
+        setMenuOpen(false);
+      }
+    };
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => document.removeEventListener('mousedown', handleClickOutside);
+  }, []);
+
+  const filteredUsers = input
+    ? users.filter((user) =>
+        user.fullName.toLowerCase().includes(input.toLowerCase())
+      )
+    : users;
+
+  useEffect(() => {
+    getUsers();
+  }, [onlineUsers]);
+
+  return (
+    <div
+      className={`bg-gradient-to-b from-indigo-900 to-blue-950 h-full p-5 rounded-r-xl overflow-y-scroll text-white ${
+        selectedUser ? 'max-md:hidden' : ''
+      }`}
+    >
+      <div className='p-5'>
+        {/* Header */}
+        <div className='flex justify-between items-center relative'>
+          <img src={assets.logo} alt='logo' className='max-w-40' />
+          <div className='relative' ref={menuRef}>
+            <img
+              src={assets.menu_icon}
+              alt='Menu'
+              className='max-h-5 cursor-pointer'
+              onClick={() => setMenuOpen(!menuOpen)}
+            />
+            {menuOpen && (
+              <div className='absolute top-full right-0 z-20 w-32 p-5 mt-2 rounded-md bg-indigo-800 border border-gray-600 text-gray-100 shadow-md'>
+                <p
+                  onClick={() => {
+                    navigate('/profile');
+                    setMenuOpen(false);
+                  }}
+                  className='cursor-pointer text-sm'
+                >
+                  Edit Profile
+                </p>
+                <hr className='my-2 border-t border-gray-500' />
+                <p
+                  onClick={() => {
+                    logout();
+                    setMenuOpen(false);
+                  }}
+                  className='cursor-pointer text-sm'
+                >
+                  Logout
+                </p>
+              </div>
+            )}
+          </div>
+        </div>
+
+        {/* Search Box */}
+        <div className='bg-indigo-800 rounded-full flex items-center gap-2 py-3 px-4 mt-5'>
+          <img src={assets.search_icon} alt='Search' className='w-3' />
+          <input
+            onChange={(e) => setInput(e.target.value)}
+            type='text'
+            className='bg-transparent border-none outline-none text-white text-xs placeholder-gray-300 flex-1'
+            placeholder='Search User...'
+          />
+        </div>
+      </div>
+
+      {/* User List */}
+      <div className='flex flex-col'>
+        {filteredUsers.map((user, index) => (
+          <div
+            key={index}
+            onClick={() => {
+              setSelectedUser(user);
+              setUnseenMessages((prev) => ({
+                ...prev,
+                [user._id]: 0,
+              }));
+            }}
+            className={`relative flex items-center gap-2 p-2 pl-4 rounded cursor-pointer max-sm:text-sm ${
+              selectedUser?._id === user._id ? 'bg-indigo-700/50' : ''
+            }`}
+          >
+            <img
+              src={user?.profilePic || assets.avatar_icon}
+              alt='avatar'
+              className='w-[35px] aspect-[1/1] rounded-full'
+            />
+            <div className='flex flex-col leading-5'>
+              <p>{user.fullName}</p>
+              {onlineUsers.includes(user._id) ? (
+                <span className='text-green-400 text-xs'>Online</span>
+              ) : (
+                <span className='text-gray-400 text-xs'>Offline</span>
+              )}
+            </div>
+
+            {unseenMessages[user._id] > 0 && (
+              <p className='absolute top-4 right-4 text-xs h-5 w-5 flex justify-center items-center rounded-full bg-blue-500/60'>
+                {unseenMessages[user._id]}
+              </p>
+            )}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
